4c2e4a25b4e7019cd8004ce24f24cae95273bac6
CT449 <aib1909880@student.ctu.edu.vn>
2023-02-18 17:24:40 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +11,18 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcone to contact book application."});
 
 });
-app.use("/api/contacts", contactsRouter);
+
+app.use("/api/contacts", contactsRouter); 
+
+//handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((req, res, next) => {
+    return res.status(error.statusCode || 500).json ({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports= app;
