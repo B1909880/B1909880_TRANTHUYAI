da9be213ed054cb7110db9e7dfb348a24166e176
CT3000N16 <tranthuyai012001@gamil.com>
2023-02-23 15:19:42 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+
 const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
-
+const app = express();
 app.use(cors());
 app.use(express.json());
 
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    return res.status(error.statusCode || 500).json ({
+    return res.status(error.statusCode || 500).json({
         message: error.message || "Internal Server Error",
     });
 });
